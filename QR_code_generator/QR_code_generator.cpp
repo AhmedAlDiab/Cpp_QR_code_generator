1a0d58b7f32c96ee96d939c0be97dbeee61f54da
Ahmed Abdulkarim <ahmedaldiabdev@gmail.com>
2026-02-05 23:58:51 +0200

    Add project files.

---
+++ QR_code_generator/QR_code_generator.cpp
@@ -0,0 +1,11 @@
+﻿// QR_code_generator.cpp : Defines the entry point for the application.
+
+#include "QR_code_generator.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
